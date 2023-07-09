24c3e68194a9f3a7eb606f88b74c719fb0f76cf1
Faisal00001 <faisalosman798@gmail.com>
2023-07-10 02:29:13 +0600

    create server

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log("je")
-           console.log("je")
+/*
+  *Title: Uptime Monitoring Application
+*/
+//Dependencies
+const http=require('http');
+//app object module scaffolding
+const app={};
+//configuration
+app.config={
+    port:3000
+};
+//Create server
+app.createServer=()=>{
+    const server=http.createServer(app.handleReqRes);
+    server.listen(app.config.port,()=>{
+        console.log(`Listening to port ${app.config.port}`)
+    })
+}
+app.handleReqRes =(req,res)=>{
+    //response handle
+    res.end('Hello world');
+}
+//start the server
+app.createServer();
