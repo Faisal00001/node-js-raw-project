ccdbb6d6875c59284f504a2c1ddc4c820a2f3772
Faisal00001 <faisalosman798@gmail.com>
2023-07-10 14:31:28 +0600

    Added parsing request section

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 */
 //Dependencies
 const http=require('http');
+const url=require('url');
+const {StringDecoder}=require('string_decoder');
 //app object module scaffolding
 const app={};
 //configuration
@@ -17,8 +19,28 @@ app.createServer=()=>{
     })
 }
 app.handleReqRes =(req,res)=>{
+    //requesting handiling
+    //get the url and parse it
+    const parsedUrl=url.parse(req.url,true)
+    const path=parsedUrl.pathname;
+    const trimmedPath=path.replace(/^\/+|\/+$/g,'')
+    console.log(trimmedPath);
+    const method=req.method.toLowerCase();
+    const queryStringObject=parsedUrl.query;
+    const headers=req.headers;
+    const decoder=new StringDecoder('utf-8');
+    let realData='';
+    req.on('data',(buffer)=>{
+        realData+=decoder.write(buffer)
+    })
+    req.on('end',()=>{
+        realData+=decoder.end();
+        console.log(realData);
+        res.end('Hello programmers');
+
+    })
     //response handle
-    res.end('Hello world');
+   
 }
 //start the server
 app.createServer();
